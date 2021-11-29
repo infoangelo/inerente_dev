6050aa575ba7f530241b1bbf5bf69f25840b1daf
Angelo Miranda <infoangelo@gmail.com>
2021-11-29 12:33:00 -0300

    Add classes Course, Lesson, Notebook, Comment and field XP to class user

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from app import app
 
 db = SQLAlchemy(app)
@@ -10,6 +11,60 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     role = db.Column(db.String(120), nullable=False)
+    xp = db.Column(db.Integer)
+    courses = db.relationship('Course', backref='instructor', lazy=True)
+    lessons = db.relationship('Lesson', backref='instructor', lazy=True)
+    notebooks = db.relationship('Notebook', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(500), unique=True, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    lessons = db.relationship('Lesson', backref='course', lazy=True)
+
+    def __repr__(self):
+        return '<Course %r>' % self.title
+
+
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(1200), nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comments = db.relationship('Comment', backref='lesson', lazy=True)
+
+    def __repr__(self):
+        return '<Lesson %r>' % self.title
+
+
+class Notebook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Notebook %r>' % self.title
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(500), unique=True, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Comments %r>' % self.comment
