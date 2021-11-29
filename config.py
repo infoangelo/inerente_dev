a194a4de233e7547984c9e6b04c8b0c40f476532
Angelo Miranda <infoangelo@gmail.com>
2021-11-29 08:49:46 -0300

    Add Gunicorn to requirements and Procfile

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://root:teste@localhost/inerente'
-SECRET_KEY = 'NossoChaveSuperSecreta'
+import os
+
+SQLALCHEMY_DATABASE_URI = os.getenv('DB_CONN')
+SECRET_KEY = os.getenv('SECRET_KEY')
